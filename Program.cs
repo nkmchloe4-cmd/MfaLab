ff67d33ef9d012c3c600ceeb41f6c64296534459
ChloeNkm <nakamanyachloe4@gmail.com>
2026-08-26 17:41:12 +0200

    Account lockout implemented and verified - locks after 5 failed attempts

--- Program.cs
+++ Program.cs
@@ -35,12 +35,11 @@ builder.Services.AddIdentityCore<ApplicationUser>(options =>
         options.SignIn.RequireConfirmedAccount = false;
         options.Stores.SchemaVersion = IdentitySchemaVersions.Version3;
 
-        // TODO steg 7: aktivera kontolåsning här. Samma options-objekt som i
-        // uppgiftstexten, även om vi registrerar Identity med AddIdentityCore
+        //aktivera kontolåsning här, även om vi registrerar Identity med AddIdentityCore
         // i stället för AddIdentity.
-        // options.Lockout.MaxFailedAccessAttempts = 5;
-        // options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(15);
-        // options.Lockout.AllowedForNewUsers = true;
+        options.Lockout.MaxFailedAccessAttempts = 5;
+        options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(15);
+        options.Lockout.AllowedForNewUsers = true;
     })
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddSignInManager()
